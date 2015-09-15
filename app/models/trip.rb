d712dd66be1894c3c84e03625ab237f933acf364
Matthias Chia <mchia2@gmail.com>
2015-09-15 22:09:00 +0800

    added layout

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,11 +1,9 @@
 class Trip < ActiveRecord::Base
   has_many :pois #use @trip.pois to retrive pois
   has_many :restaurants
-  has_many :country_code
   validates :place, presence: true
   belongs_to :user
   geocoded_by :place
-  geocoded_by :country_code
   has_many :subregion_options
   after_validation :geocode
 
